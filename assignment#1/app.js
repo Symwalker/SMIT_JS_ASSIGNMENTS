44146f50774778982f99f2748b1722db62655661
user name <99411978+Symwalker@users.noreply.github.com>
2023-04-07 19:39:06 +0500

    first assignment

---
+++ assignment#1/app.js
@@ -0,0 +1,106 @@
+// Question 1
+// let num1 = +prompt("enter first number")
+// let num2 = +prompt("enter second number")
+// let num;
+// if (num1 > num2) {
+//     num = num1;
+// }else{
+//     num = num2;
+
+// }
+// console.log(num);
+
+// Question 2
+// let showSign = +prompt("enter your number")
+// let sign;
+// if (showSign >= 0) {
+//     sign = "+"
+// }else{
+//     sign = "-"
+// }
+// console.log(sign);
+
+// Question 3
+// let num1 = +prompt("enter first number")
+// let num2 = +prompt("enter second number")
+// let num3 = +prompt("enter third number")
+// let num4 = +prompt("enter fourth number")
+// let num5 = +prompt("enter fifth number")
+// let num;
+
+// if (num1 > num2) {
+//     num = num1
+// }
+// else if (num2 > num3) {
+//     num = num2
+// }
+// else if (num3 > num4) {
+//     num = num3
+// }
+// else if (num4 > num5) {
+//     num = num4
+// }
+// else  {
+//     num = num5
+// }
+// console.log(num);
+
+// Question 4
+// for(let i = 0; i <=15;i++){
+//     if (i%2===0) {
+//         console.log("even" , i);
+//     }else{
+//         console.log("odd" , i);
+//     }
+// }
+
+
+// Question 5
+// let marks = +prompt("enter your total marks");
+// let grade;
+
+// if (marks >= 0 && marks <= 100) {
+//     if (marks >= 90) {
+//         grade = "A"
+//     }
+//     else if (marks >= 80) {
+//         grade = "B"
+//     }
+//     else if (marks >= 70) {
+//         grade = "C"
+//     }
+//     else if (marks >= 60) {
+//         grade = "D"
+//     }
+//     else {
+//         grade = "F"
+//     }
+//     console.log(grade);
+// }
+
+// Question 6
+// for(let i = 1; i<=100; i++){
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("fizzbuzz");
+//     }else if(i%5==0){
+//         console.log("buzz");
+//     }
+//     else if(i%3==0){
+//         console.log("fizz");
+
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// Question 7
+// let string = ""
+// for (let i = 1; i <= 5;i++) {
+//     for(let j = 0; j<i ;j++ ){
+//         string += "*"
+//         // console.log(j + "\n");
+//     }
+//     string += "\n"
+// }
+// console.log(string);
